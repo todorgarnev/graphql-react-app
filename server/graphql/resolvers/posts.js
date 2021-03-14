bd360ae1a888de219c4edd740bb26cb1fa33eb67
Todor Garnev <tgarnev@abv.bg>
2021-03-14 16:20:03 +0200

    Added a condition if body of createPost is empty.

--- server/graphql/resolvers/posts.js
+++ server/graphql/resolvers/posts.js
@@ -29,6 +29,11 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
+
+      if (body.trim() === "") {
+        throw new Error("Post body must not be empty");
+      }
+
       const newPost = new Post({
         body,
         user: user.id,
